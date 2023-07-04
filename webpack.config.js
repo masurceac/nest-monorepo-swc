0cff15e040fed4a7f20899401cf8cac28f882bdf
Serghei Masurceac <masurceac@gmail.com>
2023-07-04 10:33:13 +0300

    chore: add comment to webpack process exit

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const swcDefaultConfig =
   require('@nestjs/cli/lib/compiler/defaults/swc-defaults').swcDefaultsFactory()
     .swcOptions;
 
+// When the config is loaded, this line should close the app.
+// However, this doesn't happen when using require from above.
 process.exit(1);
 
 module.exports = {
